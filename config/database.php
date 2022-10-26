d002b926d4bb10d1b7de4d3d29b1879d67878136
danoux10 <danybarbe10@gmail.com>
2022-10-26 13:58:14 +0200

    test

--- config/database.php
+++ config/database.php
@@ -1,9 +1,10 @@
 <?php
 $host = 'localhost';
+//$host = '109.17.151.168';
 $dbname = 'hebdowin';
 $username = 'root';
-//$password = 'Danoux10&*';
-$password = 'password';
+$password = 'Danoux10&*';
+//$password = 'password';
 
 try{
 	$bdd= new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", "$username", "$password");
